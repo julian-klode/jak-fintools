e87d6909558a5f8a98d336e1281d58cb8c20fc19
Julian Andres Klode <jak@jak-linux.org>
2020-01-08 23:17:30 +0100

    import_traderepublic: handle tax refund

--- bin/import_traderepublic.py
+++ bin/import_traderepublic.py
@@ -43,6 +43,7 @@ def get_lines(
 
 
 def main(argv: typing.List[str]) -> None:
+    # pylint: disable=too-many-locals,too-many-branches,too-many-statements
     """Main entry point"""
 
     for path in argv[1:]:
@@ -54,6 +55,7 @@ def main(argv: typing.List[str]) -> None:
         date = None
         isin = None
         taxes = decimal.Decimal(0)
+        typ = None
 
         backlog: typing.List[str] = []
 
@@ -62,6 +64,17 @@ def main(argv: typing.List[str]) -> None:
         ) as proc:
             lines = get_lines(proc.stdout, backlog)
             for line in lines:
+                if not typ:
+                    for word in (
+                        "WERTPAPIERABRECHNUNG",
+                        "KONTOAUSZUG",
+                        "DEPOTAUSZUG",
+                        "STEUERABRECHNUNG",
+                    ):
+                        if word in line:
+                            typ = word
+                            break
+
                 if re.match("[0-9]{2}.[0-9]{2}.[0-9]{4}", line) and not date:
                     date = datetime.datetime.strptime(line, "%d.%m.%Y")
                 elif line.startswith("ISIN:") or re.match("^[A-Z0-9]{12}$", line):
@@ -71,12 +84,36 @@ def main(argv: typing.List[str]) -> None:
                     price = dec(next(lines).strip().split()[0])
                 elif line == "Fremdkostenzuschlag":
                     costs = dec(next(lines).strip().split()[0])
-                elif line in ("Kapitalertragssteuer", "Solidaritätszuschlag"):
+                elif line in (
+                    "Kapitalertragssteuer",
+                    "Kapitalertragsteuer",
+                    "Solidaritätszuschlag",
+                ):
                     taxes += dec(next(lines).strip().split()[0])
+                elif line in (
+                    "Kapitalertragsteuer Optimierung",
+                    "Solidaritätszuschlag Optimierung",
+                ):
+                    taxes -= dec(next(lines).strip().split()[0])
                 elif line == "GESAMT" and not total:
                     total = dec(next(lines).strip().split()[0])
                 elif line == "GESAMT" and total:
                     total_with_costs = dec(next(lines).strip().split()[0])
+
+        if typ == "STEUERABRECHNUNG":
+            assert taxes
+            assert total
+            assert date
+            assert not count
+            assert not price
+            assert not total_with_costs
+            assert not isin
+            print(
+                f'{date.strftime("%Y/%m/%d")} Steueroptimierung  ; {path.split("/")[-1]}'
+            )
+            print(f"    expenses:taxes                     {taxes}€")
+            print(f"    assets:bank:savings:traderepublic  {total}€")
+            continue
 
         assert count
         assert price
